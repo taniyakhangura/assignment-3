e578618cc1d83d3901553f1a55e621405530cfbf
Taniya Khangura <khan8370@mylaurier.ca>
2021-04-19 14:56:01 -0400

    added basic order

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = class extends Page {
     constructor(){
         super({title:"Home", sName:"Richard Hildred"});
     }
-    render(sPage) {
-        const oJson = fetch("https://prog8110winter2021section2-default-rtdb.firebaseio.com/meals.json").json();
-        console.log(oJson);
-        let sResult = "<h1>Upcoming Popup Meals</h1>";
-        Object.keys(oJson).map((key) => {
+   render(sPage) {
+       const oJson = fetch("https://UX308App-default-rtdb.firebaseio.com/meals.json").json();
+       console.log(oJson);
+       let sResult = "<h1>Upcoming Popup Meals</h1>";
+       Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
             oEntity.id = key;
@@ -17,14 +17,13 @@ module.exports = class extends Page {
             <h2>${oEntity.title}</h2>
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
             <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
-            <input type="hidden" name="title" value="${oEntity.title}" />
-            <input type="hidden" name="price" value="21" />
-            <input type="tel" placeholder="enter your number" name="telephone"/>
-            <button type="submit">Order now</button>
+            <form action="http://localhost:3001/payment" method="post">
+            <input type="hidden" value="${oEntity.title}" />
+            <input type="tel" placeholder="Enter your number" />
+            <button type="Submit">Order now</button>
             </form>
             `;
-        });
-        return sResult;
+       });
+       return sResult;
     }
 }
