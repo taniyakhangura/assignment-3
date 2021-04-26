897ba7f1f2df400718884c1f904d10e2b913614c
Taniya Khangura <khan8370@mylaurier.ca>
2021-04-26 15:18:37 -0400

    added changes and commited

--- index.js
+++ index.js
@@ -3,23 +3,27 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"Home", sName:"Richard Hildred"});
+        super({title:"Home", sName:"Taniya Khangura"});
     }
    render(sPage) {
-       const oJson = fetch("https://UX308App-default-rtdb.firebaseio.com/meals.json").json();
+       const oJson = fetch("https://ux308final-3813f-default-rtdb.firebaseio.com/meals.json").json();
        console.log(oJson);
-       let sResult = "<h1>Upcoming Popup Meals</h1>";
+       let sResult = "<h1>Diwali Craft Kits</h1>";
        Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
             oEntity.id = key;
             sResult += `
-            <h2>${oEntity.title}</h2>
+            <h2>${oEntity.title} </h2> 
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
+            <p>${oEntity.price}</p>
             <p>${oEntity.full_description}</p>
-            <form action="http://localhost:3001/payment" method="post">
-            <input type="hidden" value="${oEntity.title}" />
-            <input type="tel" placeholder="Enter your number" />
+            <p>Deadline to Order: ${oEntity.date}</p>
+            <p>This Kit Includes: ${oEntity.materials}</p>
+            <form action="https://ux308.herokuapp.com/payment" method="post">
+            <input type="hidden" name="title" value="${oEntity.title}" />
+            <input type="hidden" name="price" value="${oEntity.price}" />
+            <input type="tel" placeholder="Enter your number" name="telephone"/>
             <button type="Submit">Order now</button>
             </form>
             `;
